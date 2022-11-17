e9c0cda0d69c0ad668e8ff0a9490b43d22cbb698
tldk42 <tldk47@hanmail.net>
2022-11-17 20:07:22 +0900

    refactor : send함수 promise로 묶어줌

--- src/app.js
+++ src/app.js
@@ -37,37 +37,33 @@ class Bot {
 
   /** 문자열 분석 후 그룹지어 봇이 어떤 작업을 할지 전달합니다. */
   static send(text, channel) {
-    let instruction;
-
-    if (!Number.isNaN(Number(text))) instruction = square(this.#rtm, text, channel);
-
-    switch (text) {
-      case !Number.isNaN(Number(text)):
-        instruction = square(this.#rtm, text, channel);
-        break;
-      case 'hi':
-        instruction = greeting(this.#rtm, channel);
-        break;
-      case '학사일정':
-        // TODO: feature 2
-        break;
-      case '오늘 밥 뭐야':
-        // TODO: feature 3
-        break;
-      case '*학부':
-        // TODO: feature 4
-        break;
-      default:
-        instruction = 'undefined';
-        break;
-    }
     return new Promise((resolve, reject) => {
-      if (instruction === 'undefined') reject(this.#rtm.sendMessage('Say hi or number', channel));
+      let instruction;
+
+      switch (text) {
+        case 'hi':
+          instruction = greeting(this.#rtm, channel);
+          break;
+        case '학사일정':
+          // TODO: feature 2
+          break;
+        case '오늘 밥 뭐야':
+          // TODO: feature 3
+          break;
+        case '*학부':
+          // TODO: feature 4
+          break;
+        default:
+          instruction = 'undefined';
+          break;
+      }
+      if (!Number.isNaN(Number(text))) resolve(square(this.#rtm, text, channel));
+      else if (instruction === 'undefined') reject(this.#rtm.sendMessage('Say hi or number', channel));
       else resolve(instruction);
     });
   }
 }
 
 module.exports = {
-  Bot,
+  Bot
 };
