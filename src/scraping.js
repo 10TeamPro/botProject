ae74cfc9c23f23297dacaeb79a2b01a4937ca772
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-11-20 21:30:00 +0900

    chore : Update scraping.js - 주석 삭제 #38

--- src/scraping.js
+++ src/scraping.js
@@ -2,13 +2,13 @@ const { spawn } = require('child_process');
 
 const result = spawn('python', ['foodData.py']);
 
-// stdout의 'data'이벤트리스너로 실행결과를 받음
+// 'data'이벤트리스너로 실행결과를 받음
 result.stdout.on('data', (data) => {
   const text = data.toString('utf-8');
   console.log(text);
 });
 
-// 에러 발생 시, stderr의 'data'이벤트리스너로 실행결과를 받음
+// 에러 발생 시, 'data'이벤트리스너로 실행결과를 받음
 result.stderr.on('data', (data) => {
   const text = data.toString('utf8');
   console.log(text);
