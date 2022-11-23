585492c9c908bd550473daa35e90a914d577da4e
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-11-23 20:08:17 +0900

    Update square.js

--- src/square.js
+++ src/square.js
@@ -1,7 +1,6 @@
 const square = (rtm, text, channel) => {
   /* eslint-disable no-console */
   console.log('square : ');
-  console.log(text);
   /* eslint-enable no-console */
   return rtm.sendMessage(`${text * text}`, channel);
 };
