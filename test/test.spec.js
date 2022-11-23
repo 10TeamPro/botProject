06b0f3e47f21b471004652f98621a2e6e8e86807
MscMin <113351941+MscMin@users.noreply.github.com>
2022-11-24 04:02:53 +0900

    Update test.spec.js

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
 const sayHello = require('../src/test').SayHello;
 
+
+
+
+
 describe('App test!', () => {
   it('sayHello should return "hello"', () => {
     assert.equal(sayHello(), 'hello');
