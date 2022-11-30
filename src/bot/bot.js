e82c886eca232dcd8bccf07e59fb5022812dab05
tldk42 <tldk47@hanmail.net>
2022-11-30 21:24:38 +0900

    refactor : Class Bot 코드 리팩터링 - 불필요한 변수 제거 - 코드 간략화

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -9,24 +9,10 @@ class Bot {
   /** 응답 가능한 단계 */
   responseLevel;
 
-  /** 학사 일정 텍스트 파일 */
-  static departmentSchedule;
-
-  /**  학식 메뉴 텍스트 파일 */
-  static menuSchedule;
-
-  /** 학과 사무실 정보 */
-  static departmentInfo;
-
-  /** 시스템이 켜질 때  한번만 데이터를 미리 로드합니다. */
   constructor(token) {
     this.rtm = null;
     this.responseLevel = 1;
     this.rtm = new RTMClient(token, { logLevel: LogLevel.INFO });
-
-    // this.#departmentSchedule = fs.readFileSync('rsc/data/schedule.txt').toString().split('\n');
-    // this.#menuSchedule = fs.readFileSync('rsc/data/menu.txt').toString().split('\n');
-    // this.#departmentInfo = fs.readFileSync('rsc/data/info.txt').toString().split('\n');
   }
 
   /** 모니터링 시작 */
