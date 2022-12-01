d46b79f209e321dc1d67989c4e9fa2fb46302af6
tldk42 <tldk47@hanmail.net>
2022-12-01 20:59:05 +0900

    fix : 충돌 해결..

--- src/square.js
+++
@@ -1,9 +0,0 @@
-const square = (rtm, text, channel) => {
-  /* eslint-disable no-console */
-  console.log('square : ');
-  console.log(text);
-  /* eslint-enable no-console */
-  return rtm.sendMessage(`${text * text}`, channel);
-};
-
-module.exports = square;
---
+++ test/greeting.spec.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const greeting = require('../src/greeting');
+
+const greetArray = [
+  "Hi. We're here to help",
+  'Good to see you!',
+  'Hi',
+];
+
+describe('인사 테스트', () => {
+  it('Case 0', () => {
+    const stub = sinon.stub(Math, 'random').returns(0 / 3);
+    stub();
+    assert.equal(greeting(), greetArray[0]);
+    Math.random.restore();
+  });
+  it('Case 1', () => {
+    const stub = sinon.stub(Math, 'random').returns(1 / 3);
+    stub();
+    assert.equal(greeting(), greetArray[1]);
+    Math.random.restore();
+  });
+  it('Case 2', () => {
+    const stub = sinon.stub(Math, 'random').returns(2 / 3);
+    stub();
+    assert.equal(greeting(), greetArray[2]);
+    Math.random.restore();
+  });
+});
