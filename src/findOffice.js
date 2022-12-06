8cc1e0a998ff86a40d79a99d0088ed511a2fed63
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-12-06 10:15:40 +0900

    feat : Update findOffice.js - levenshtein 기능 추가

--- src/findOffice.js
+++ src/findOffice.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const closest = require('fastest-levenshtein');
 
 const dataInput = fs
   .readFileSync('./rsc/data/dept.txt', 'utf-8')
@@ -8,13 +9,18 @@ const dataInput = fs
 /* 파일에서 학과의 사무실 위치 탐색 */
 function findOffice(department) {
   let office = 'undefined';
+  let realDepart = 'undefined';
   let buffer = '';
 
-  /* 입력으로 들어온 학과의 사무실을 찾았을 경우 */
+  /* 입력으로 들어온 학과의 사무실 찾기 */
   dataInput.forEach((element) => {
     if (element.includes(department)) {
       buffer = element.split('-');
       office = buffer[1].trim();
+    } else {
+      realDepart = closest(department, element);
+      realDepart = realDepart[0].trim();
+      office = `${department}를 말씀하시는건가요? ${realDepart} 입니다.`;
     }
   });
 
