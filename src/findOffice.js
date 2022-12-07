986e59b0b7d8a2e857d854ad86fc149c410c86b9
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-12-07 22:25:08 +0900

    feat : Update findOffice.js - levenshtein 구현 완료

--- src/findOffice.js
+++ src/findOffice.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const levenshtein = require('fast-levenshtein');
 
 const dataInput = fs
   .readFileSync('./rsc/data/dept.txt', 'utf-8')
@@ -7,14 +8,35 @@ const dataInput = fs
 
 /* 파일에서 학과의 사무실 위치 탐색 */
 function findOffice(department) {
+  let distance = 0;
+  let min = 100;
   let office = 'undefined';
+  let realDepart = 'undefined';
   let buffer = '';
 
-  /* 입력으로 들어온 학과의 사무실을 찾았을 경우 */
   dataInput.forEach((element) => {
+    buffer = element.split('-');
+    const trimmedDept = buffer[0]
+      .trim()
+      .toLowerCase()
+      .split(' ');
+
+    // 입력으로 들어온 학과의 사무실을 찾았을 경우
     if (element.includes(department)) {
-      buffer = element.split('-');
       office = buffer[1].trim();
+    }
+    // 입력과 일치하는 이름이 없을 경우 비슷한 이름 탐색
+    else {
+      trimmedDept.forEach((word) => {
+        distance = levenshtein.get(department, word);
+        if (distance < min && word !== 'and') {
+          min = distance;
+          realDepart = buffer[0].trim();
+          office = buffer[1].trim();
+        }
+      });
+
+      office = `${realDepart}를 말씀하시는건가요? ${office} 입니다.`;
     }
   });
 
